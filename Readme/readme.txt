485804a27eec8b64a773d05ab3790c696902f238
damiancombis <damian.combiss@hotmail.com>
2023-04-17 21:53:36 -0300

    actividad numero 2 // DAMIAN COMBIS

--- Readme/readme.txt
+++ Readme/readme.txt
@@ -3,11 +3,34 @@ Actividad Num°1
 En este proyecto intento replicar la pagina de Dexter (indumentaria).
 Elegi la indumentaria porque en algun momento me gustaria trabajar haciendo este tipo de paginas.
 
-En la solapa "main.js" creo la funcion de cuponDescuento15 pero no la utilizo (planeo usarla mas adelante)
-tambien esta la funcion validacion que lo que hace es tomar la respuesta de un usuario y devolver "1" para "Si" "2" para "no" y "3" para cualquier otra cosa que no sea "SI" o "NO".
+La funcion validacion que lo que hace es tomar la respuesta de un usuario y devolver "1" para "Si" "2" para "no" y "3" para cualquier otra cosa que no sea "SI" o "NO".
 
 La funcion validacion la utilizo dentro de un FOR para darle 3 oportunidades al cliente de introducir bien la respuesta
 con un alert que le avisa cuantos intentos le van quedando.
 tambien podria poner un alert que le avise que tiene que responder SI O NO. 
 
 Para finalizar si la respuesta es SI hace una cosa y si la respuesta es NO hace otra.
+
+Actividad Num°2
+
+En esta actividad utilize las respuestas de la actividad anterior.
+
+En el caso de que respondiera * 1 ("SI") * Le pide al cliente que ingrese su usuario (valido el nombre de usuario con some) en el caso de escribir mal su nombre de usuario se lo vuelve a pedir(tiene 3 intentos) pero si el nombre de usuario esta bien le pide la contraseña (valido la contraseña de usuario con some)  en el caso de escribir mal su contraseña se lo vuelve a pedir(tiene 3 intentos). en todo caso de ser correcto le da la bienvenida o lo dirige a la pagina.
+
+En el caso de que respondiera * 2 ("NO") * le consulta al cliente si quiere crear un usuario.
+
+Si el cliente responde * ("SI") * le pide que ingrese el nombre de usuario, ese nombre de usuario es validado con la funcion cantidadCaracteres(esta funcion toma una variable y utiliza el metodo length para comparar tiene mas de 4 caracteres) el resultado de esa comparacion TRUE or FALSE se guarda dentro de la varible TEST
+
+linea 218 - Si TEST es * FALSE * le vuelve a pedir que ingrese su nombre de usuario indicando anteriormente al cliente que debe tener mas de 4 digitos dandole con un ciclo for 3 intentos. En el caso de ingresar bien el usuario, le pide la contraseña y utilizo la variable TESTPASSWORD para la respuesta de CANTIDADCARACTERES (usando el mismo metodo de validacion que use para validar el nombre de usuario). En el caso de que TESTPASSWORD sea TRUE carga el usuario al array de objetos 
+
+linea 275 - Si TEST es * TRUE * le pide la contraseña y utilizo la variable TESTPASSWORD para la respuesta de CANTIDADCARACTERES (usando el mismo metodo de validacion que use para validar el nombre de usuario). En el caso de que TESTPASSWORD sea TRUE carga el usuario al array de objetos 
+
+linea 328 utilizo un for of para mostrar los usuarios ya creados y tambien el creado por el cliente
+
+linea 338 le consulto al cliente si quiere ver los productos de oferta. utilizo la funcion validacion para su respuesta en el caso  de responder si le muestro las ofertas con un for of y separo los productos con un join para que se pueda distinguir con mas claridad
+
+linea 381 le consulto al cliente si quiere ver los ultimos lanzamientos. utilizo la funcion validacion para su respuesta en el caso  de responder si le muestro los productos con la funcion porCadaUno que toma el array y lo muestra con el console.log
+
+
+
+
--- Script/main.js
+++ Script/main.js
@@ -1,94 +1,474 @@
-function resta(num1,num2){
-	return num1 - num2 ;
-}
-function suma(num1,num2){
-	return num1 + num2 ;
-}
-function cuponDescuento15(precio){
-		let descuento ;
-		descuento = (precio * 15)/100;
-		return precio - descuento;
-}
-
-function validacion(respuestaUsuario) {
- 
-  if (respuestaUsuario.toUpperCase() === "SI") {
-      return  1;
-  }
-  else if (respuestaUsuario.toUpperCase() === "NO") {
-      return 2 ;
-  }
-      return 3; 
-}
 
 
-//*************************************************************************************************************************//
-//*************************************************************************************************************************//
-
-
-
-
-let respuesta = 0;
-respuesta =   validacion(prompt("Bienvenido a King-Lion, ¿usted tiene un usuario?")) ;
-alert(respuesta);
-
-
-
-if (respuesta >= 3) {
-
-      alert("ERROR");
-
-       for ( i = 1; i <= 3; i++) {
-
-                    respuesta =   validacion(prompt("Bienvenido a King-Lion, ¿usted tiene un usuario?")) ;
-
-
-             if (respuesta >= 3) {
-
-                 alert("ERROR fatal num " + i );
-
-                  if (i === 3) {
-                                alert("Se te acabaron los intentos");
-                              }
-                              
-             }
-             else if( respuesta === 1 ){
-
-                break;
-
-             }
-             else if( respuesta === 2 ){
-
-               
-                break;
-
-             }
+    function validacion(respuestaUsuario) {
+     
+      if (respuestaUsuario.toUpperCase() === "SI") {
+          return  1;
       }
+      else if (respuestaUsuario.toUpperCase() === "NO") {
+          return 2 ;
+      }
+          return 3; 
+    }
+
+    function cantidadCaracteres(variable){
+
+            return variable.length > 4;
+
+    }
+
+     function porCadaUno(array, funcion){
+
+        for(const elemento of array){
+          
+          funcion(elemento);
+      }
+
+    }
+
+
+    //*************************************************************************************************************************//
+    //*************************************************************************************************************************//
+
+    class ListadoZapatillas {
+      constructor(nombre, precio, genero) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.genero = genero.toUpperCase();
+        
+      }
+
+     
+    }
+    let misProductos = [
+      
+                  {nombre: "Zapatillas Air Max" , precio: 50000.20 },
+                  {nombre: "Zapatillas Diadora" , precio: 25000.99 },
+                  {nombre: "Zapatillas reebok" , precio: 45000.50 }
+    ];
+
+
+
+
+
+
+    //*************************************************************************************************************************//
+    //*************************************************************************************************************************//
+
+    const PRODUCTOSOFERTA = ["Zapatillas Penalty $10000", "Zapatillas Jaguar $ 5000", "Zapatillas NIKE $20000"];
+
+    class Usuarios {
+      constructor(nombre, contra){
+        this.nombre = nombre.toUpperCase();
+        this.contra = contra;
+          
+      }
+      hablar(){
+         console.log("Su nombre de usuario es: " + this.nombre + " / Su contraseña es: " + this.contra);
+      }
+    }
+     const misUsuarios = [];
+
+     const testUsuarioNombre = "CHAMAN";
+     const testUsuarioContra = 1234;
+     
+     const unUsuario = new Usuarios(testUsuarioNombre, testUsuarioContra);
+     misUsuarios.push(unUsuario);
+
+
+
+    //*************************************************************************************************************************//
+    //*************************************************************************************************************************//
+
+    let respuesta = 0;
+    respuesta =   validacion(prompt("Bienvenido a King-Lion, ¿usted tiene un usuario?")) ;
+
+
+
+    if (respuesta >= 3) {
+
+          console.log("ERROR");
+            let i=1;
+           for ( i ; i <= 3; i++) {
+
+                        respuesta =   validacion(prompt("Bienvenido a King-Lion, ¿usted tiene un usuario?")) ;
+
+
+                 if (respuesta >= 3) {
+                    console.log("Respuesta incorrecta, responder SI o NO");
+                     console.log("ERROR NUMERO " + i );
+
+                      if (i === 3) {
+                                    alert("Se te acabaron los intentos");
+                                  }
+                                  
+                 }
+                 else if( respuesta === 1 ){
+
+                    break;
+
+                 }
+                 else if( respuesta === 2 ){
+
+                   
+                    break;
+
+                 }
+          }
+    }
+
+
+    /********************************************************************************************************/
+    /*******************************************************************************************************/
+
+     let loginUsuario;
+     
+     let respuestaCrearUsuario;
+     let crearUsuario;
+
+    if (respuesta === 1) {
+
+         let loginUsuario = prompt("Ingrese su usuario");
+         let UsuarioExiste;
+         UsuarioExiste = misUsuarios.some(
+           (unUsuario) => unUsuario.nombre === loginUsuario.toUpperCase()
+          );
+
+       
+          if (!UsuarioExiste) {
+
+          console.log("ERROR");
+          let i=1;
+           for ( i ; i <= 3; i++) {
+
+                       let loginUsuario = prompt("Ingrese su usuario");
+
+                          UsuarioExiste = misUsuarios.some(
+                          (unUsuario) => unUsuario.nombre === loginUsuario.toUpperCase());
+
+
+                 if (!UsuarioExiste) {
+                    alert("Nombre de Usuario incorrecto");
+                     console.log("ERROR NUMERO " + i );
+
+                      if (i === 3) {
+                                    alert("Se te acabaron los intentos");
+                                  }
+                } else if( UsuarioExiste ){
+
+                    break;
+
+                 }
+            }
+          }
+
+          let contraValida
+          let contraUsuario = true;
+
+          if (UsuarioExiste) {
+
+                    let contraValida = prompt("ingrese su contraseña");
+
+                    let contraUsuario = misUsuarios.some(
+                    (unUsuario) => unUsuario.contra ===  parseInt(contraValida) );
+
+                   if (contraUsuario) {
+                        alert("bienvenido a King-Lion");
+                       }
+
+              if (!contraUsuario) {
+
+                      console.log("ERROR");
+                      let i = 1;
+                  for ( i ; i <= 3; i++) {
+
+                       let contraValida = prompt("ingrese su contraseña");
+
+                       let contraUsuario = misUsuarios.some(
+                        (unUsuario) => unUsuario.contra ===  parseInt(contraValida) );
+
+                       if (contraUsuario) {
+                        alert("bienvenido a King-Lion");
+                        break;
+
+                    }else if (!contraUsuario) {
+                          alert(" contraseña incorrecta");
+                         console.log("ERROR NUMERO " + i );
+                    }if (i === 3) {
+                                    alert("Se te acabaron los intentos");
+                                  }
+            }
+          }
+        }
+
+        }else if( respuesta === 2 ){
+                   
+                   let test
+                   let respuestaCrearUsuario = 0;
+                   respuestaCrearUsuario  =   validacion(prompt("¿Quiere crear un Usuario ahora?")) ;
+
+             
+                 if( respuestaCrearUsuario === 1 ){           
+
+                        let crearUsuario = prompt("Ingrese el nombre de usuario");
+                         let test = cantidadCaracteres(crearUsuario);
+                         console.log(test);
+
+
+                        if(!test){
+
+                            alert("El nombre de usuario debe tener mas de 4 digitos");
+
+                            let i = 1;
+                            for ( i  ; i <= 3; i++) {
+
+                              
+                              crearUsuario = prompt("Ingrese el nombre de usuario");
+                              test = cantidadCaracteres(crearUsuario);
+
+
+                              if (test){
+
+                                   alert("bienvenido a king-lion " + crearUsuario); 
+                                   crearPassword = prompt("ingrese su contraseña..(La contraseña tiene que tener mas de 4 digitos)");
+                                  let testPasword = cantidadCaracteres(crearPassword);
+
+                                  if(!testPasword){ 
+
+                            let i = 1;
+                             let crearPassword = 0;
+
+                            alert("Respuesta incorrecta, la contraseña debe tener por lo menos 5 caracteres");
+                            console.log("ERROR NUMERO " + i );
+
+                            for ( i = 1; i <= 3; i++) {
+
+                              
+                              let crearPassword = prompt("ingrese su contraseña..(La contraseña tiene que tener mas de 4 digitos)");
+                              let testPasword = cantidadCaracteres(crearPassword);
+
+
+                              if (testPasword){
+
+                                 const unUsuario = new Usuarios(crearUsuario, crearPassword);
+                                  misUsuarios.push(unUsuario);
+                                  unUsuario.hablar();
+                                  break;
+
+                              }else if (i === 3) {
+                              
+                                            alert("Se te acabaron los intentos");
+                                            break;
+                           }
+                          } 
+                       }else{
+
+                            const unUsuario = new Usuarios(crearUsuario, crearPassword);
+                            misUsuarios.push(unUsuario);
+                            unUsuario.hablar();
+                          } 
+                                  break;
+
+                              }
+                        }
+
+                      }else{
+
+                         alert("Bienvenido a king-lion " + crearUsuario); 
+                         crearPassword = prompt("ingrese su contraseña..(La contraseña tiene que tener mas de 4 digitos)"); /* aca no*/
+                         let testPasword = cantidadCaracteres(crearPassword);
+
+                         if(!testPasword){ 
+
+                            let i = 1;
+                             let crearPassword = 0;
+     
+                            alert("Respuesta incorrecta, La contraseña debe tener por lo menos 5 caracteres"); 
+                            console.log("ERROR NUMERO " + i );
+
+                            for ( i = 1; i <= 3; i++) {
+
+                              
+                              let crearPassword = prompt("ingrese su contraseña..(La contraseña tiene que tener mas de 4 digitos)");
+                              let testPasword = cantidadCaracteres(crearPassword);
+
+
+                              if (testPasword){
+
+                                 const unUsuario = new Usuarios(crearUsuario, crearPassword);
+                                 misUsuarios.push(unUsuario);
+                                 unUsuario.hablar();
+                                  break;
+
+                              }else if (i === 3) {
+                              
+                                            alert("Se te acabaron los intentos");
+                                            break;
+                           }
+                          } 
+                       }else{
+
+                            const unUsuario = new Usuarios(crearUsuario, crearPassword);
+                            misUsuarios.push(unUsuario);
+                            unUsuario.hablar();
+
+                          } 
+                      }
+                    }  
+                  }       
+                     
+                     
+      
+
+
+
+
+      let i = 1 ;
+
+    for(const unUsuario of misUsuarios){
+        
+        console.log("El nombre de Usuario numero " + i + " es: " + unUsuario.nombre);
+        console.log("La contraseña del Usuario numero " + i + " es: " + unUsuario.contra );
+        i++; 
+    }
+
+  /*    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  */
+
+
+  respuesta =   validacion(prompt("Quiere ver los productos de oferta?")) ;
+
+  if (respuesta >= 3) {
+
+        console.log("ERROR");
+
+         for ( i = 1; i <= 3; i++) {
+
+                      respuesta =   validacion(prompt("Quiere ver los productos de oferta?")) ;
+
+
+               if (respuesta >= 3) {
+                  console.log("Respuesta incorrecta, responder SI o NO");
+                   console.log("ERROR NUMERO " + i );
+
+                    if (i === 3) {
+                                  alert("Se te acabaron los intentos");
+                                }
+                                
+               }
+               else if( respuesta === 1 ){
+
+                  break;
+
+               }
+               else if( respuesta === 2 ){
+
+                 
+                  break;
+
+               }
+        }
+  }
+                     
+  if (respuesta === 1) {
+
+   for(const unProducto of PRODUCTOSOFERTA){
+        
+        console.log(PRODUCTOSOFERTA.join("; "));
+    }
+
+  }
+
+  respuesta = validacion(prompt("Quiere ver los ultimos lanzamientos?")) ;
+
+  if (respuesta >= 3) {
+
+        console.log("ERROR");
+
+         for ( i = 1; i <= 3; i++) {
+
+                      respuesta =   validacion(prompt("Quiere ver los ultimos lanzamientos?")) ;
+
+
+               if (respuesta >= 3) {
+                  console.log("Respuesta incorrecta, responder SI o NO");
+                   console.log("ERROR NUMERO " + i );
+
+                    if (i === 3) {
+                                  alert("Se te acabaron los intentos");
+                                }
+                                
+               }
+               else if( respuesta === 1 ){
+
+                  break;
+
+               }
+               else if( respuesta === 2 ){
+
+                 
+                  break;
+
+               }
+        }
+  }
+
+  if (respuesta === 1) {
+
+   porCadaUno(misProductos,console.log);
+
+
+  }
+
+
+
+
+
+
+     
+    
+
+
+          
+        
+
+
+
+
+/********************************************************************************************************/
+/*******************************************************************************************************/
+
+
+
+
+/**
+
+class ListadoZapatillas {
+  constructor(nombre, precio, genero) {
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.genero = genero.toUpperCase();
+    this.vendido = false;
+
+
+  }
 }
 
-if (respuesta === 1) {
+let misProductos = [];
 
-    let loginUsuario = 0 ;
-    loginUsuario = prompt("Ingrese su usuario");
+let respuestaCargaProducto = "SI";
 
-}else if( respuesta === 2 ){
+do {
+  let nombre = prompt("Ingrese el nombre del producto");
+  let precio = prompt("Ingrese el precio del producto");
+  let genero = prompt("Ingrese el genero del producto");
+  let unProducto = new ListadoZapatillas(nombre, precio, genero);
+  misProductos.push(unProducto);
+  console.log("--> La cantidad de productos en mí lista de Zapatillas, es de ", misProductos.length);
+  respuestaCargaProducto = prompt("¿Quieres seguir añadiendo más productos? SI/NO");
+} while (respuestaCargaProducto.toUpperCase() !== "NO");
 
-               let crearUsuario = 0 ;
-                crearUsuario = prompt("Invente su usuario");
+//agregar validacion de error de respuesta
 
-             }
+console.log("--> Esta es la lista de todos los productos que cargué", misProductos);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+**/
